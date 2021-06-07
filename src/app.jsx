3ca0879ec2c8c0d88fbd4721378fbf30b8120bf4
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2021-06-07 23:44:28 +0200

    project update

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,26 @@
-const App = () => {
-  return app;
-};
+import { motion, useCycle } from 'framer-motion'
+import { useRef } from 'react'
+import { useDimensions } from './hooks/use-dimentions'
+import styled from 'styled-components'
 
-export default App;
+const Nav = styled(motion.nav)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 300px;
+`
+
+const App = () => {
+  const containerRef = useRef(null)
+  const { height } = useDimensions(containerRef)
+  const [isOpen, toggleOpen] = useCycle(false, true)
+
+  return (
+    <Nav initial={false} custom={height} ref={containerRef}>
+      nav {height}
+    </Nav>
+  )
+}
+
+export default App
--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -1,16 +1,25 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
-const styled = { createGlobalStyle };
+const styled = { createGlobalStyle }
 
-const GlobalStyle = styled.createGlobalStyle`
+const GlobalStyles = styled.createGlobalStyle`
   body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-      "Helvetica Neue", sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
-`;
 
-export default GlobalStyle;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(180deg, #0055ff 0%, rgb(0, 153, 255) 100%);
+    overflow: hidden;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export default GlobalStyles
